588d44f284e96f954e841e056a0c68704c9ea7db
Dalton Guimaraes <daltonguim@gmail.com>
2022-12-17 23:28:26 -0400

    List for packages

---
+++ List_of_useful_packages_in_R.R
@@ -0,0 +1,83 @@
+getwd()
+setwd("C:/Users/02762829224/Documents/DTSP/COTES")
+getwd()
+
+packageStatus()
+
+# list of useful packages
+# Link: https://joaobispoti.com.br/index.php/2019/01/14/lista-rapida-de-pacotes-r-uteis/
+
+# For load data to Database
+
+install.packages("RMySQL")
+install.packages("RPostgreSQL")
+install.packages("RSQLite")
+install.packages("XLConnect")
+install.packages("foreigen")
+
+# For handle Data
+
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("lubridate")
+
+# For preview Data
+
+install.packages("ggplot2")
+install.packages("ggvis")
+install.packages("rgl")
+install.packages("htmlwidgets")# install using file.zip
+
+# To model data
+
+install.packages("anova")# The "anova" package has been replaced by: ANOVAIREVA, ANOVAShiny, ANOVAShiny2.
+install.packages("mgcv")
+install.packages("lme4")
+install.packages("nlme")
+install.packages("randomForest")
+install.packages("vcd")
+install.packages("glmnet")
+install.packages("survival")
+install.packages("caret")
+
+# For report results
+
+install.packages("brilhate") # package ‘brilhate’ is not available for this version of R
+
+install.packages("rmarkdown") #  package ‘Rmarkdown’ is not available for this version of R
+
+install.packages("xtable")
+
+# For time series and financial data
+
+install.packages("zoo")
+install.packages("xts")
+install.packages("quantmod")
+
+# To write high-performance R code
+
+install.packages("Rcpp")
+install.packages("data.table")
+install.packages("paralelo")
+
+# To work with the web
+
+install.packages("xml")
+install.packages("jsonlite")
+install.packages("httr")
+
+# To write your own R packages
+
+install.packages("devtools")
+install.packages("testthat")
+install.packages("roxygen2")
+
+# list of useful packages
+# Link: https://blog.curso-r.com/posts/2017-02-21-top10-pacotes-para-data-science/
+
+install.packages("purrr")
+install.packages("caret")
+install.packages("knitr")
+install.packages("lubridate")
+install.packages("magrittr")
